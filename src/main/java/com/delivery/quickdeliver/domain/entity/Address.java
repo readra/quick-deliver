ca50ba39a0944c263a9521eff803f24af002d6c7
jykim <yong6302@gmail.com>
2025-12-10 19:33:33 +0900

    1. Prometheus 의존성 문제 해결
    build.gradle에서 Prometheus 관련 의존성 주석 처리
    application.yml에 Auto-configuration 제외 설정 추가
    
    2. 엔티티 매핑 문제 해결
    
    Address 엔티티의 contactPhone 필드에 @Column(name = "address_contact_phone") 추가
    Merchant 엔티티의 contactPhone과 중복되는 컬럼명 충돌 해결
    
    🚀 이제 사용 가능한 URL들:
    
    로그인 페이지: http://localhost:8080/login.html
    라이더 대시보드: http://localhost:8080/rider-dashboard.html
    백오피스 대시보드: http://localhost:8080/backoffice-dashboard.html
    실시간 배송 지도: http://localhost:8080/delivery-map.html
    Swagger UI: http://localhost:8080/swagger-ui.html
    H2 Console: http://localhost:8080/h2-console
    
    📌 테스트 계정:
    
    관리자: admin / admin123
    백오피스: backoffice / backoffice123
    라이더1: rider1 / rider123
    라이더2: rider2 / rider123

--- src/main/java/com/delivery/quickdeliver/domain/entity/Address.java
+++ src/main/java/com/delivery/quickdeliver/domain/entity/Address.java
@@ -1,5 +1,6 @@
 package com.delivery.quickdeliver.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 
@@ -14,6 +15,10 @@ public class Address {
     private String detailAddress;
     private Double latitude;
     private Double longitude;
+    
+    @Column(name = "address_contact_name")
     private String contactName;
+    
+    @Column(name = "address_contact_phone")
     private String contactPhone;
 }
